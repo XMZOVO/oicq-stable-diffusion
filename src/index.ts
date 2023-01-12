a03eb76ea1f3e6c21d61524bbc99872d7126142b
Roy <2980999208@qq.com>
2023-01-12 23:04:12 +0800

    add img2img message

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,17 @@ import type { GroupMessageEvent, PrivateMessageEvent } from 'oicq'
 import { createClient } from 'oicq'
 import axios from 'axios'
 import sha256 from 'crypto-js/sha256'
+import { encode } from 'node-base64-image'
 
-const account = 3558195590
+const account = 3203223297
 const client = createClient(account)
 const messageList = [] as string[]
+const options = {
+  string: true,
+  headers: {
+    'User-Agent': 'my-app',
+  },
+}
 interface Result {
   prefix?: string
   params: Array<string>
@@ -47,8 +54,17 @@ client.on('message.private', async (e) => {
 async function reply(e: PrivateMessageEvent | GroupMessageEvent) {
   const result = await processMessage(e.raw_message)
   if (result.remaining) {
-    e.reply(`妈妈正在生成${result.params.includes('-hd') ? '高清' : ''}图片：${result.remaining}`, true)
-    const base64 = await generate(result.remaining, result.params)
+    let base64
+    if (e.message[1] && e.message[1].type === 'image' && e.message[1].url) {
+      e.reply(`妈妈正在生成图片：${result.remaining}`, true)
+      const image = await encode(e.message[1].url, options) as string
+      base64 = await generateFromImg(result.remaining, result.params, image)
+    }
+    else {
+      e.reply('妈妈生不动了', true)
+      return
+    }
+    // base64 = await generate(result.remaining, result.params)
 
     const reply = await e.reply([{
       type: 'image',
@@ -62,7 +78,7 @@ async function processMessage(message: string): Promise<Result> {
   const result: Result = { params: [] }
   const string = message
 
-  const pattern = /^(sdAI|sdTrans)\s+((?:-\w+\s?)*)?(.+)?$/
+  const pattern = /^(sdAI|sdTrans)\s+((?:-[\w\.]+\s?)*)?([^\[]+)?/
   const match = pattern.exec(string)
   if (match) {
     result.prefix = match[1]
@@ -106,11 +122,11 @@ function truncate(q: string) {
   return q.substring(0, 10) + len + q.substring(len - 10, len)
 }
 
-async function generate(prompt: string, params: Array<string>): Promise<string> {
+async function generate(prompt: string, _: Array<string>): Promise<string> {
   const res = await axios.post('http://127.0.0.1:7861/sdapi/v1/txt2img', {
     steps: '30',
-    width: 512 * (params.includes('-hd') ? 1.5 : 1),
-    height: 640 * (params.includes('-hd') ? 1.5 : 1),
+    width: 512,
+    height: 640,
     cfg_scale: 10,
     sampler_index: 'DDIM',
     prompt: `${prompt}`,
@@ -125,4 +141,34 @@ async function generate(prompt: string, params: Array<string>): Promise<string> 
           `,
   })
   return res.data.images[0]
+}
+
+async function generateFromImg(prompt: string, params: Array<string>, img64: string) {
+  const res = await axios.post('http://127.0.0.1:7861/sdapi/v1/img2img', {
+    steps: '30',
+    width: 410,
+    height: 720,
+    resize_mode: 1,
+    cfg_scale: 21,
+    denoising_strength: getParamsValue(params, '-den') !== undefined ? Math.min(Math.max(parseFloat(getParamsValue(params, '-den')!), 0), 1) : 0.64,
+    sampler_index: 'DDIM',
+    prompt: `${prompt}`,
+    init_images: [
+      img64,
+    ],
+    negative_prompt:
+          `(worst quality, low quality:1.4), logo, text, monochrome,
+          (((pornographic))),((asshole)),(vaginal),(((pussy))),(((penis))),(((nude))),(((anus)))
+          `,
+  })
+  return res.data.images[0]
+}
+
+function getParamsValue(params: Array<string>, prifix: string) {
+  const found = params.find(str => str.includes(prifix))
+
+  if (found) {
+    const value = found.split(prifix)[1]
+    return value !== '' ? value : undefined// "1" or "2"
+  }
 }
